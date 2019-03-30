b4680558e21a0e59f4b6e15aa5098ec0c74917c1
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-03-30 17:34:01 +0100

    fix(test): activate non running test

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,4 +3,5 @@ from tests import assert_run
 
 def test_init_project_in_empty_folder(tmpdir):
     """Run --init in empty folder"""
-    assert_run('baw --init xcd "I Like This Project"', cwd=tmpdir)
+    with assert_run('baw --init xcd "I Like This Project"', cwd=tmpdir):
+        pass
