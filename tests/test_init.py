3f8358760427dd55f2cd203c67797a3b27573ae5
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-03-31 10:51:06 +0200

    test(doc, init): test --init generates doc pages and run --doc
    Check that --init generates required documentation pages. Afterwards run
    --doc to ensure doc generation works.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,25 @@
+from os.path import exists
+from os.path import join
+
 from tests import assert_run
+from tests import skip_longrunning
 
 
 def test_init_project_in_empty_folder(tmpdir):
-    """Run --init in empty folder"""
+    """Run --init in empty folder
+
+    Intitialize project and check if documentation is generated."""
+    with assert_run('baw --init xcd "I Like This Project"', cwd=tmpdir):
+        assert exists(join(tmpdir, 'docs/pages/bugs.rst'))
+        assert exists(join(tmpdir, 'docs/pages/changelog.rst'))
+        assert exists(join(tmpdir, 'docs/pages/readme.rst'))
+        assert exists(join(tmpdir, 'docs/pages/todo.rst'))
+
+
+@skip_longrunning
+def test_doc_command(tmpdir):
+    """Run --doc command to generate documentation."""
     with assert_run('baw --init xcd "I Like This Project"', cwd=tmpdir):
         pass
+    with assert_run('baw --doc', cwd=tmpdir):
+        assert exists(join(tmpdir, 'docs/html'))
