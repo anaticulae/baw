2f3cd950d96034689978e4be2ee780a160028f0b
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-02 20:11:15 +0200

    feat(test): install baw in virtual environment
    On virtual environment, the installation of current baw-tool is
    required.

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+#==============================================================================
+# C O P Y R I G H T
+#------------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+#==============================================================================
+"""The test-startup starts with this file. If running on virtual environment,
+installing baw is required. If baw-commands are skipped, cause of having very
+fast feedback when only developing functions, baw will not installed.
+"""
+from sys import argv
+
+from baw.runtime import run_target
+from baw.runtime import VIRTUAL_FOLDER
+from baw.utils import logging
+from baw.utils import profile
+from baw.utils import ROOT
+from baw.utils import SUCCESS
+from tests import FAST
+
+
+def install_baw():
+    """Install baw-runtime to virtual environment"""
+    logging('Install baw...')
+    command = 'python setup.py install'
+    completed = run_target(ROOT, command, ROOT, verbose=True)
+    msg = 'Could not install required baw'
+
+    assert completed.returncode == SUCCESS, msg
+
+    completed = run_target(ROOT, 'baw --help', ROOT, verbose=False)
+    assert completed.returncode == 0
+
+
+RUN_VIRTUAL = VIRTUAL_FOLDER in ''.join(argv)
+if RUN_VIRTUAL and not FAST:
+    with profile():
+        install_baw()
