fdeed29acac5a1ce0a013fa5814f2acbedbc916e
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-03 15:39:54 +0200

    fix(template): add missing import, remove longname from README template

--- baw/resources.py
+++ baw/resources.py
@@ -25,7 +25,7 @@ assert exists(WORKSPACE_TEMPLATE), 'No template %s' % WORKSPACE_TEMPLATE
 GIT_IGNORE_TEMPLATE = join(TEMPLATES, '.gitignore')
 assert exists(GIT_IGNORE_TEMPLATE), 'No gitignore %s' % GIT_IGNORE_TEMPLATE
 
-README = """# $_SHORT_$ - $_NAME_$
+README = """# $_SHORT_$
 """
 
 CHANGELOG = """# Changelog
@@ -86,7 +86,19 @@ Indices and tables
 
 REQUIREMENTS = ""
 
-INIT = """__version__ = '0.0.0'
+INIT = """\
+#==============================================================================
+# C O P Y R I G H T
+#------------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+#==============================================================================
+
+import os
+
+__version__ = '0.0.0'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 """
