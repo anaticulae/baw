d0585add2923791fce4bed7b8ea5675bd98774a9
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-10 09:27:23 +0200

    test(sync): add parameterized test for synchronizing packages

---
+++ tests/test_sync.py
@@ -0,0 +1,16 @@
+#==============================================================================
+# C O P Y R I G H T
+#------------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+#==============================================================================
+import pytest
+from baw.cmd.sync import sync
+from baw.utils import ROOT
+
+
+@pytest.mark.parametrize('virtual', [False, True])
+def test_sync(virtual):
+    sync(ROOT, virtual=virtual, verbose=False)
