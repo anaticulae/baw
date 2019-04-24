9ce1e6d43493cc1229611c2377011cf84531566a
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-24 02:29:52 +0200

    text(main): test --version and --help command to increase testcov

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,37 @@
-def test_install():
-    pass
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import sys
+
+from baw import main
+from pytest import mark
+from pytest import raises
+
+
+@mark.parametrize('command', [
+    ['-h'],
+    ['-v'],
+    ['--format'],
+])
+def test_run_command(monkeypatch, command):
+    """Run help and version and format command to reach basic test coverage"""
+    with monkeypatch.context() as context:
+        # Remove all environment vars
+        # baw is removed as first arg
+        context.setattr(sys, 'argv', ['baw'] + command)
+
+        with raises(SystemExit) as result:
+            main()
+        result = str(result)
+        assert 'SystemExit: 0' in result, result
+
+
+def assert_success(result):
+    result = str(result)
+    assert 'SystemExit: 0' in result, result
