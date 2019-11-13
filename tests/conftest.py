55270b4f34212b81b00192670f634c47ed51e4e2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-11-13 16:57:48 +0100

    fix(test): require baw installation in virtual test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,15 @@
+import os
+
+import baw
+import baw.runtime
+import baw.utils
+
 pytest_plugins = 'pytester'  # pylint: disable=invalid-name
+
+if not 'PYTEST_XDIST_WORKER' in os.environ:
+    # master process only
+    if 'VIRTUAL' in os.environ:
+        cleaninstall = 'pip uninstall baw -y && python setup.py install'
+        completed = baw.runtime._run(command=cleaninstall, cwd=baw.utils.ROOT)
+
+        assert completed.returncode == baw.utils.SUCCESS
