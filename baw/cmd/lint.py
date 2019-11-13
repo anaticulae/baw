f5b69f38ff54e15baf6a74ec87fb1bb11e220a18
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-11-13 08:16:42 +0100

    fix(linter): lint test directory only if it exists

--- baw/cmd/lint.py
+++ baw/cmd/lint.py
@@ -8,6 +8,8 @@
 # =============================================================================
 """The purpose of this module is to run python linter safely."""
 
+import os
+
 from baw.config import sources
 from baw.resources import RCFILE_PATH
 from baw.runtime import run_target
@@ -15,8 +17,13 @@ from baw.utils import logging
 
 
 def lint(root: str, verbose: bool = False, virtual: bool = False):
-    lint_sources = ' '.join(sources(root))
-    cmd = 'pylint %s tests --rcfile=%s ' % (lint_sources, RCFILE_PATH)
+    code = ' '.join(sources(root))
+
+    testpath = os.path.join(root, 'tests')
+    linttest = testpath if os.path.exists(testpath) else ''
+
+    cmd = f'pylint {code} {linttest} --rcfile={RCFILE_PATH}'
+
     completed = run_target(
         root,
         cmd,
