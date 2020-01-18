22827c55f818c0c23e5cecd6c4a863310ebaaa58
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-01-18 13:42:27 +0100

    chore(test): ensure that baw is installed on virtual environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
-import os
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
 
-import baw
+import os
+import subprocess
+
 import baw.runtime
 import baw.utils
 
@@ -9,7 +18,21 @@ pytest_plugins = 'pytester'  # pylint: disable=invalid-name
 if not 'PYTEST_XDIST_WORKER' in os.environ:
     # master process only
     if 'VIRTUAL' in os.environ:
-        cleaninstall = 'pip uninstall baw -y && python setup.py install'
-        completed = baw.runtime._run(command=cleaninstall, cwd=baw.utils.ROOT)
 
-        assert completed.returncode == baw.utils.SUCCESS
+        def run(cmd):
+            result = subprocess.run(
+                cmd,
+                cwd=baw.utils.ROOT,
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                universal_newlines=True,
+                timeout=5.0,
+            )
+            return result
+
+        # ensure that baw is installed
+        COMPLETED = run('python setup.py install')
+        assert COMPLETED.returncode == baw.utils.SUCCESS, COMPLETED
+
+        COMPLETED = run('where baw')
+        assert '.virtual' in COMPLETED.stdout.split()[0], COMPLETED
