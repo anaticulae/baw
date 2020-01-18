4b74689fdb7b6d3e0c5f4be61634dd1a40136829
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-01-18 14:51:18 +0100

    chore(test): remove timeout to run test successful under heavy loaded environment

--- tests/test_format.py
+++ tests/test_format.py
@@ -49,7 +49,7 @@ def test_regression_format_keep_single_list(simpleproject):  # pylint:disable=W0
     baw.utils.file_create(path, source)
     assert os.path.exists(path)
 
-    completed = subprocess.run('baw --format', timeout=10.0)
+    completed = subprocess.run('baw --format')
     assert completed.returncode == baw.utils.SUCCESS
 
     read = baw.utils.file_read(path)
