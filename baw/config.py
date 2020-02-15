226d845daabe7073df9be4aa2b955cf3cb02c9aa
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-15 08:10:57 +0100

    refactor(config): order due expected usage
    Extend interface documentation.
    
    Hint: Moving and refactoring in one step is not a good idea.

--- baw/config.py
+++ baw/config.py
@@ -16,6 +16,89 @@ import baw.utils
 PROJECT_PATH = '.baw/project.config'
 
 
+def name(root: str):
+    assert os.path.exists(root)
+    cfg = os.path.join(root, PROJECT_PATH)
+    _, name_ = project(cfg)
+    # escape '
+    name_ = name_.replace("'", r'\'')
+    return name_
+
+
+def shortcut(root: str) -> str:
+    """Read short project name out of project config.
+
+    Args:
+        root(str): path to project root
+    Returns:
+        shortname of the project
+    """
+    assert os.path.exists(root)
+    cfg = os.path.join(root, PROJECT_PATH)
+    short, _ = project(cfg)
+    return short
+
+
+def create_config(root: str, shortname: str, longname: str):
+    """Create project-config in .baw folder
+
+    Args:
+        root(str): path to expected project root
+        shortname(str): short name eg. 3 chars long of project
+        longname(str): project-name which is used in generated
+    """
+    assert os.path.exists(root)
+    cfg = configparser.ConfigParser()
+    cfg['project'] = {'short': shortname, 'name': longname}
+    output = os.path.join(root, PROJECT_PATH)
+    with open(
+            output,
+            mode='w',
+            encoding=baw.utils.UTF8,
+            newline=baw.utils.NEWLINE,
+    ) as fp:
+        cfg.write(fp)
+
+
+def commands(root: str) -> dict:
+    """Determine commands to run out of project config.
+
+    Args:
+        root(str): project root
+    Returns:
+        dict{name, command}: dict with commands to execute
+    """
+    assert os.path.exists(root)
+
+    path = os.path.join(root, PROJECT_PATH)
+    assert os.path.exists(path)
+
+    cfg = config(path)
+    try:
+        # TODO: DIRTY, goto standard lib
+        return {item: cfg['run'][item] for item in cfg['run']}
+    except KeyError:
+        return {}
+
+
+def minimal_coverage(root: str) -> int:
+    """Read percentage of required test coverage
+
+    Args:
+        root(str): path to project root
+    Returns:
+        percentage of required test coverage
+    """
+    assert os.path.exists(root)
+    cfg = config(os.path.join(root, PROJECT_PATH))
+
+    try:
+        min_coverage = int(cfg['tests']['minimal_coverage'])
+    except KeyError:
+        min_coverage = 20
+    return min_coverage
+
+
 def config(path: str):
     if not os.path.exists(path):
         raise ValueError('Configuration %s does not exists' % path)
@@ -59,84 +142,3 @@ def sources(path: str) -> list:
         source = []
     source.insert(0, cfg['project']['short'])
     return source
-
-
-def shortcut(root: str):
-    """Read short project name out of project configuration
-
-    Args:
-        path(str): path to project root
-    Returns:
-        shortname of the project"""
-    assert os.path.exists(root)
-    cfg = os.path.join(root, PROJECT_PATH)
-    short, _ = project(cfg)
-    return short
-
-
-def name(root: str):
-    assert os.path.exists(root)
-    cfg = os.path.join(root, PROJECT_PATH)
-    _, name_ = project(cfg)
-    # escape '
-    name_ = name_.replace("'", r'\'')
-    return name_
-
-
-def minimal_coverage(root: str) -> int:
-    """Read percentage of required test coverage
-
-    Args:
-        root(str): path to project root
-    Returns:
-        percentage of required test coverage
-    """
-    assert os.path.exists(root)
-    cfg = config(os.path.join(root, PROJECT_PATH))
-
-    try:
-        min_coverage = int(cfg['tests']['minimal_coverage'])
-    except KeyError:
-        min_coverage = 20
-    return min_coverage
-
-
-def commands(root: str):
-    """Determine commands to run out of project config
-
-    Args:
-        root(str): project root
-    Returns:
-        dict{name, command}: dict with commands to execute
-    """
-    assert os.path.exists(root)
-
-    path = os.path.join(root, PROJECT_PATH)
-    assert os.path.exists(path)
-
-    cfg = config(path)
-    try:
-        # TODO: DIRTY, goto standard lib
-        return {item: cfg['run'][item] for item in cfg['run']}
-    except KeyError:
-        return {}
-
-
-def create_config(root: str, shortname: str, longname: str):
-    """Create project-config in .baw folder
-
-    Args:
-        shortcut(str): short name eg. 3 chars long
-        name(str): project-name which is used in generated documents
-    """
-    assert os.path.exists(root)
-    cfg = configparser.ConfigParser()
-    cfg['project'] = {'short': shortname, 'name': longname}
-    output = os.path.join(root, PROJECT_PATH)
-    with open(
-            output,
-            mode='w',
-            encoding=baw.utils.UTF8,
-            newline=baw.utils.NEWLINE,
-    ) as fp:
-        cfg.write(fp)
