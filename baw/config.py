11b3620536ee496ded42f662b6fae340ab435f22
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-15 07:55:22 +0100

    style(config): organize imports

--- baw/config.py
+++ baw/config.py
@@ -1,20 +1,25 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
 """Handle access to project configuration which is stored in .baw-folder."""
-from configparser import ConfigParser
-from os.path import exists
-from os.path import isfile
-from os.path import join
+import configparser
+import os
 
-from baw.utils import NEWLINE
-from baw.utils import UTF8
+import baw.utils
 
 PROJECT_PATH = '.baw/project.config'
 
 
 def config(path: str):
-    if not exists(path):
+    if not os.path.exists(path):
         raise ValueError('Configuration %s does not exists' % path)
-    cfg = ConfigParser()
-    with open(path, mode='r', encoding=UTF8) as fp:
+    cfg = configparser.ConfigParser()
+    with open(path, mode='r', encoding=baw.utils.UTF8) as fp:
         cfg.read_file(fp)
     return cfg
 
@@ -32,12 +37,11 @@ def sources(path: str):
     Returns:
         list with source folder of project
     """
-
     # support accessing the config directly or due the project path
-    if not exists(path) or not isfile(path):
-        potential_config = join(path, PROJECT_PATH)
-        if exists(potential_config) and isfile(potential_config):
-            path = potential_config
+    if not os.path.exists(path) or not os.path.isfile(path):
+        potential_cfg = os.path.join(path, PROJECT_PATH)
+        if os.path.exists(potential_cfg) and os.path.isfile(potential_cfg):
+            path = potential_cfg
     cfg = config(path)
     try:
         source = cfg['project']['source'].splitlines()
@@ -52,18 +56,17 @@ def shortcut(root: str):
 
     Args:
         path(str): path to project root
-
     Returns:
         shortname of the project"""
-    assert exists(root)
-    cfg = join(root, PROJECT_PATH)
+    assert os.path.exists(root)
+    cfg = os.path.join(root, PROJECT_PATH)
     short, _ = project_name(cfg)
     return short
 
 
 def name(root: str):
-    assert exists(root)
-    cfg = join(root, PROJECT_PATH)
+    assert os.path.exists(root)
+    cfg = os.path.join(root, PROJECT_PATH)
     _, name_ = project_name(cfg)
     # escape '
     name_ = name_.replace("'", r'\'')
@@ -78,8 +81,8 @@ def minimal_coverage(root: str) -> int:
     Returns:
         percentage of required test coverage
     """
-    assert exists(root)
-    cfg = config(join(root, PROJECT_PATH))
+    assert os.path.exists(root)
+    cfg = config(os.path.join(root, PROJECT_PATH))
 
     try:
         min_coverage = int(cfg['tests']['minimal_coverage'])
@@ -96,10 +99,10 @@ def commands(root: str):
     Returns:
         dict{name, command}: dict with commands to execute
     """
-    assert exists(root)
+    assert os.path.exists(root)
 
-    path = join(root, PROJECT_PATH)
-    assert exists(path)
+    path = os.path.join(root, PROJECT_PATH)
+    assert os.path.exists(path)
 
     cfg = config(path)
     try:
@@ -116,9 +119,14 @@ def create_config(root: str, shortname: str, longname: str):
         shortcut(str): short name eg. 3 chars long
         name(str): project-name which is used in generated documents
     """
-    assert exists(root)
-    cfg = ConfigParser()
+    assert os.path.exists(root)
+    cfg = configparser.ConfigParser()
     cfg['project'] = {'short': shortname, 'name': longname}
-    output = join(root, PROJECT_PATH)
-    with open(output, mode='w', encoding=UTF8, newline=NEWLINE) as fp:
+    output = os.path.join(root, PROJECT_PATH)
+    with open(
+            output,
+            mode='w',
+            encoding=baw.utils.UTF8,
+            newline=baw.utils.NEWLINE,
+    ) as fp:
         cfg.write(fp)
