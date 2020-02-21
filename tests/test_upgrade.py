9383ec1d7145656185f2c0176b5a067b7ba6ca04
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-21 20:21:23 +0100

    style(imports): organize imports

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -11,23 +11,13 @@ import textwrap
 
 import baw.cmd.upgrade
 import baw.requirements
+import baw.runtime
+import tests
+import tests.fixtures.project
 import tests.fixtures.requirements
-from baw.cmd.upgrade import available_version
-from baw.cmd.upgrade import determine_new_requirements
-from baw.cmd.upgrade import installed_version
-from baw.cmd.upgrade import upgrade_requirements
-from baw.runtime import run_target
-from baw.utils import FAILURE
-from baw.utils import REQUIREMENTS_TXT
-from baw.utils import ROOT
-from baw.utils import SUCCESS
-from baw.utils import file_append
-from baw.utils import file_create
-from baw.utils import file_read
-from baw.utils import file_remove
-from tests import skip_longrun
-from tests import skip_nonvirtual
-from tests.fixtures.project import project_example  # pylint: disable=W0611
+
+# fixture
+project_example = tests.fixtures.project.project_example  # pylint:disable=C0103
 
 NEW_VERSION_AVAILABLE = """
 utila (0.5.4)  - 0.5.4
@@ -47,33 +37,33 @@ pdfminer.six (20181108)  - 20181108
 
 
 def test_complex():
-    available = available_version(AVAILABLE_COMPLEX)
+    available = baw.cmd.upgrade.available_version(AVAILABLE_COMPLEX)
     assert available == "20181108"
 
-    installed = installed_version(AVAILABLE_COMPLEX)
+    installed = baw.cmd.upgrade.installed_version(AVAILABLE_COMPLEX)
     assert installed == "20181108"
 
 
 def test_up_to_date():
-    available = available_version(AVAILABLE_INSTALLED)
+    available = baw.cmd.upgrade.available_version(AVAILABLE_INSTALLED)
     assert available == "0.5.4"
 
-    installed = installed_version(AVAILABLE_INSTALLED)
+    installed = baw.cmd.upgrade.installed_version(AVAILABLE_INSTALLED)
     assert installed == "0.5.4"
 
 
 def test_out_of_date():
-    available = available_version(NEW_VERSION_AVAILABLE)
+    available = baw.cmd.upgrade.available_version(NEW_VERSION_AVAILABLE)
     assert available == "0.5.4"
 
-    installed = installed_version(NEW_VERSION_AVAILABLE)
+    installed = baw.cmd.upgrade.installed_version(NEW_VERSION_AVAILABLE)
     assert installed == "0.5.3"
 
 
-@skip_longrun
+@tests.skip_longrun
 def test_new_requirements():
-    result = determine_new_requirements(
-        ROOT,
+    result = baw.cmd.upgrade.determine_new_requirements(
+        baw.utils.ROOT,
         tests.fixtures.requirements.REQUIREMENTS,
     )
     # utila should allways be new than 0.5.0
@@ -87,31 +77,31 @@ iamraw
 
 
 def test_upgrading(tmpdir):
-    requirements_path = os.path.join(tmpdir, REQUIREMENTS_TXT)
+    requirements_path = os.path.join(tmpdir, baw.utils.REQUIREMENTS_TXT)
 
-    file_create(requirements_path, TEST_UPGRADE)
+    baw.utils.file_create(requirements_path, TEST_UPGRADE)
 
     # use default requirements.txt
-    upgrade_requirements(tmpdir, virtual=True)
+    baw.cmd.upgrade.upgrade_requirements(tmpdir, virtual=True)
 
-    loaded = file_read(requirements_path)
+    loaded = baw.utils.file_read(requirements_path)
     assert loaded != TEST_UPGRADE
 
 
-@skip_longrun
-@skip_nonvirtual
+@tests.skip_longrun
+@tests.skip_nonvirtual
 def test_upgrade_requirements(project_example, capsys):  # pylint: disable=W0621, W0613
     path = os.getcwd()
 
     def commit_all():
-        completed = run_target(
+        completed = baw.runtime.run_target(
             path,
             'git add . && git commit -m "Upgade requirements"',
         )
-        assert completed.returncode == SUCCESS, str(completed)
+        assert completed.returncode == baw.utils.SUCCESS, str(completed)
 
     # yapf in a higher version is provided by dev environment
-    file_append(REQUIREMENTS_TXT, 'yapf==0.10.0')
+    baw.utils.file_append(baw.utils.REQUIREMENTS_TXT, 'yapf==0.10.0')
 
     failed_test = textwrap.dedent("""\
     def test_me():
@@ -119,7 +109,7 @@ def test_upgrade_requirements(project_example, capsys):  # pylint: disable=W0621
     """)
     failingtest_path = 'tests/test_failed.py'
 
-    file_create(failingtest_path, failed_test)
+    baw.utils.file_create(failingtest_path, failed_test)
     commit_all()
 
     result = baw.cmd.upgrade.upgrade(
@@ -128,7 +118,7 @@ def test_upgrade_requirements(project_example, capsys):  # pylint: disable=W0621
         virtual=True,
         generate=False,  # do not change - see test.py/generate_only
     )
-    assert result == FAILURE
+    assert result == baw.utils.FAILURE
     stdout = capsys.readouterr().out
 
     assert stdout
@@ -136,7 +126,7 @@ def test_upgrade_requirements(project_example, capsys):  # pylint: disable=W0621
 
     # Reuse virtual environment
     # remove failling test
-    file_remove(failingtest_path)
+    baw.utils.file_remove(failingtest_path)
     commit_all()
 
     result = baw.cmd.upgrade.upgrade(
@@ -145,4 +135,4 @@ def test_upgrade_requirements(project_example, capsys):  # pylint: disable=W0621
         virtual=True,
         generate=False,  # see above
     )
-    assert result == SUCCESS
+    assert result == baw.utils.SUCCESS
