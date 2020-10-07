ebd82c685ff5043cb9a9492a86044f7d7632b351
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-07 14:54:34 +0200

    feat(upgrade): do not sync upgraded resources

--- baw/cmd/upgrade.py
+++ baw/cmd/upgrade.py
@@ -58,7 +58,7 @@ def upgrade(
             packages='dev',  # minimal requirements is required
             quiet=True,
             stash=False,
-            sync=True,  # install new packages
+            sync=not notests,  # install new packages
             test=not notests,
             testconfig=None,
             verbose=False,
