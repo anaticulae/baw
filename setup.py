328bad1e9182ab027ffa13df5c93f81d1cb89c8c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-08-19 15:54:03 +0200

    refactor(setup): cleanup setup file

--- setup.py
+++ setup.py
@@ -10,30 +10,18 @@
 """ Run this setup script to install baw on your local maschine or
 virtual environment."""
 
-import collections
 import os
 import re
 
 import setuptools
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
-
-README = open(
-    os.path.join(ROOT, 'README.md'),
-    mode='r',
-    newline='\n',
-).read()
-
-VERSION_FILE = open(
-    os.path.join(ROOT, 'baw/__init__.py'),
-    mode='r',
-    newline='\n',
-).read()
-
+with open(os.path.join(ROOT, 'README.md'), 'rt', encoding='utf8') as fp:
+    README = fp.read()
+with open(os.path.join(ROOT, 'baw/__init__.py'), 'rt', encoding='utf8') as fp:
+    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
 with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
     REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
-
-VERSION = re.search(r'__version__ = \'(.*?)\'', VERSION_FILE).group(1)
 
 if __name__ == "__main__":
     setuptools.setup(
