0abf8dcfde41c5bd31ad9b5af5ebcb506852c471
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-21 19:44:01 +0100

    fix(resource): fix template

--- baw/resources.py
+++ baw/resources.py
@@ -110,7 +110,8 @@ Progress
   pages/changelog
 
 Modules
---------------------
+-------
+
 .. toctree::
    :maxdepth: 4
 
@@ -129,7 +130,7 @@ COPYRIGHT = """\
 #==============================================================================
 # C O P Y R I G H T
 #------------------------------------------------------------------------------
-# Copyright (c) 2021-2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
 # This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
