b8f7eb1c32f0a3f26efeacde0c1de6c8209ff4ef
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-03-11 23:46:59 +0100

    style(runtime): organize imports

--- baw/runtime.py
+++ baw/runtime.py
@@ -8,28 +8,13 @@
 #==============================================================================
 
 import os
+import shutil
 import subprocess
 import sys
-from os import environ
-from os import makedirs
-from os import scandir
-from os.path import exists
-from os.path import isdir
-from os.path import join
-from shutil import rmtree
-from sys import platform
-from time import time
+import time
 
 import baw.config
 import baw.utils
-from baw.utils import FAILURE
-from baw.utils import SUCCESS
-from baw.utils import UTF8
-from baw.utils import error
-from baw.utils import file_read
-from baw.utils import file_replace
-from baw.utils import log
-from baw.utils import print_runtime
 
 VIRTUAL_FOLDER = '.virtual'
 
@@ -38,18 +23,18 @@ NO_EXECUTABLE = 127
 
 def destroy(path: str):
     """Remove virtual path recursive if path exists, do nothing."""
-    if not exists(path):
-        log('Nothing to clean, path does not exists %s' % path)
+    if not os.path.exists(path):
+        baw.utils.log('Nothing to clean, path does not exists %s' % path)
         return True
-    log('Removing virtual environment %s' % path)
+    baw.utils.log('Removing virtual environment %s' % path)
     try:
-        rmtree(path)
+        shutil.rmtree(path)
     except PermissionError as fail:
         # This error occurs, if an ide e.g. vscode uses the virtual environment
         # so removing .virtual folder is not possible.
         msg = 'Could not remove %s. Path is locked by an other application.'
-        error(fail)
-        error(msg % path)
+        baw.utils.error(fail)
+        baw.utils.error(msg % path)
         return False
     return True
 
@@ -60,7 +45,7 @@ def venv(root: str) -> str:
     virtual = os.path.join(baw.config.bawtmp(), 'venv', name)
     if os.path.exists(virtual):
         return virtual
-    outdated = join(root, VIRTUAL_FOLDER)
+    outdated = os.path.join(root, VIRTUAL_FOLDER)
     if os.path.exists(outdated):
         baw.utils.error(f'use outdated virtual: {outdated}')
         return outdated
@@ -81,12 +66,12 @@ def create(root: str, clean: bool = False, verbose: bool = False) -> int:
         SUCCESS if creating was was succesfull else FAILURE
     """
     virtual = venv(root)
-    if not exists(virtual):
-        log(f'create virtual: {virtual}')
-        makedirs(virtual, exist_ok=True)
-    if exists(virtual) and list(scandir(virtual)):
-        log(f'virtual: {virtual}')
-        return SUCCESS
+    if not os.path.exists(virtual):
+        baw.utils.log(f'create virtual: {virtual}')
+        os.makedirs(virtual, exist_ok=True)
+    if os.path.exists(virtual) and list(os.scandir(virtual)):
+        baw.utils.log(f'virtual: {virtual}')
+        return baw.utils.SUCCESS
     python = baw.config.python(root, virtual=False)
     # '--system-site-packages'
     cmd = f'{python} -m virtualenv . --copies '
@@ -98,16 +83,16 @@ def create(root: str, clean: bool = False, verbose: bool = False) -> int:
         # python 3.7
         patch_env(root)
     if process.returncode:
-        error(cmd)
-        error('While creating virutal environment:')
-        log(process.stdout)
-        error(process.stderr)
-        return FAILURE
+        baw.utils.error(cmd)
+        baw.utils.error('While creating virutal environment:')
+        baw.utils.log(process.stdout)
+        baw.utils.error(process.stderr)
+        return baw.utils.FAILURE
     if verbose:
-        log(process.stdout)
+        baw.utils.log(process.stdout)
         if process.stderr:
-            error(process.stderr)
-    return SUCCESS
+            baw.utils.error(process.stderr)
+    return baw.utils.SUCCESS
 
 
 def patch_pip(root):
@@ -117,22 +102,22 @@ def patch_pip(root):
 
     TODO: REMOVE WITH UPGRADED PIP OR PDFMINER
     """
-    log('Patching the wheel')
+    baw.utils.log('Patching the wheel')
     to_patch = os.path.join(
         venv(root),
         'Lib/site-packages/pip/_internal/wheel.py',
     )
-    if not exists(to_patch):
+    if not os.path.exists(to_patch):
         return
     template = 'for row in sorted(outrows):'
     replacement = 'for row in outrows:'
-    content = file_read(to_patch).replace(template, replacement)
-    file_replace(to_patch, content)
+    content = baw.utils.file_read(to_patch).replace(template, replacement)
+    baw.utils.file_replace(to_patch, content)
 
 
 def patch_env(root):
     path = os.path.join(venv(root), 'Scripts/activate.bat')
-    content = file_read(path)
+    content = baw.utils.file_read(path)
     content = content.split(':END')[0]  # remove content after :END
     baw.utils.file_remove(path)
     baw.utils.file_create(path, content=content)
@@ -172,7 +157,7 @@ def run_target(
     Returns:
         CompletedProcess - os process which was runned
     """
-    start = time()
+    start = time.time()
 
     try:
         cwd, skip_error_code, skip_error_message = setup_target(
@@ -182,11 +167,11 @@ def run_target(
             skip_error_message,
         )
     except ValueError as fail:
-        error(str(fail))
-        return FAILURE
+        baw.utils.error(str(fail))
+        return baw.utils.FAILURE
 
     if verbose:
-        log(command)
+        baw.utils.log(command)
 
     if virtual:
         try:
@@ -235,9 +220,9 @@ def setup_target(
 ):
     if not cwd:
         cwd = root
-    if not exists(cwd):
+    if not os.path.exists(cwd):
         raise ValueError('cwd: %s does not exists' % cwd)
-    if not isdir(cwd):
+    if not os.path.isdir(cwd):
         raise ValueError('cwd: %s is not a directory' % cwd)
     if not skip_error_code:
         skip_error_code = {}
@@ -274,17 +259,17 @@ def log_result(  # pylint:disable=R1260,R0912
     reporting = returncode and (returncode not in skip_error_code)
     if reporting:
         msg = f'Completed: `{command}` in `{cwd}` returncode: {returncode}\n'
-        error(msg)
+        baw.utils.error(msg)
 
     if completed.stdout and verbose:
-        log(completed.stdout)
+        baw.utils.log(completed.stdout)
 
     if verbose:
         if not reporting:
             # Inform, not writing to stderr
-            log(f'Completed: `{command}` in `{cwd}`\n')
+            baw.utils.log(f'Completed: `{command}` in `{cwd}`\n')
         if verbose == 2:  # TODO: Introduce VERBOSE level
-            log('Env: %s' % environ)
+            baw.utils.log('Env: %s' % os.environ)
 
     error_message = completed.stderr
     # catch stderr is None when running baw --test=pdb because the std-out/err
@@ -295,14 +280,14 @@ def log_result(  # pylint:disable=R1260,R0912
         error_message = error_message.replace(remove_skip, '')
 
     if reporting and error_message.strip():
-        error('%s' % error_message.strip())
+        baw.utils.error('%s' % error_message.strip())
     if verbose:
         if completed.stderr:
-            error(completed.stderr)
+            baw.utils.error(completed.stderr)
         if completed.stdout:
-            log(completed.stdout)
+            baw.utils.log(completed.stdout)
         if start is not None:
-            print_runtime(start)
+            baw.utils.print_runtime(start)
 
 
 def _run_virtual(
@@ -327,12 +312,12 @@ def _run_virtual(
     """
     activate = os.path.join(venv(root), 'Scripts', 'activate')
     deactivate = os.path.join(venv(root), 'Scripts', 'deactivate')
-    if not exists(activate):
+    if not os.path.exists(activate):
         msg = (f'Path `{activate}` does not exists.\n'
                'Regenerate the virtual env')
         raise RuntimeError(msg)
 
-    if platform == 'win32':
+    if sys.platform == 'win32':
         activate = f'{activate}.bat'
         deactivate = f'{deactivate}.bat'
     else:
@@ -355,13 +340,13 @@ def run(command: str, cwd: str, env=None, debugging: bool = False):
     if not isinstance(command, str):
         command = ' '.join(command)
     if env is None:  # None: Empty dict is allowed.
-        env = dict(environ.items())
+        env = dict(os.environ.items())
     # Capturering stdout and stderr reuqires PIPE in completed process.
     # Debugging with pdb due console requires no PIPE.
     process = subprocess.run(  # pylint:disable=W1510
         command,
         cwd=cwd,
-        encoding=UTF8,
+        encoding=baw.utils.UTF8,
         env=env,
         shell=True,
         stderr=None if debugging else subprocess.PIPE,
