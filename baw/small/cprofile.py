af970d061603703ba8ffccc775d215381885194a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-15 17:26:42 +0200

    feat(cprofile): add cli to ease cprofile evaluation

---
+++ baw/small/cprofile.py
@@ -0,0 +1,53 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import argparse
+import os
+import pstats
+
+import baw.utils
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    for path in files(args):
+        profile_show(path)
+    return baw.utils.SUCCESS
+
+
+def files(args):
+    result = []
+    for path in args.files:
+        if not os.path.exists(path):
+            baw.utils.error(f'file does not exists: {path}')
+            continue
+        path = os.path.abspath(path)
+        result.append(path)
+    return result
+
+
+def profile_show(path: str):
+    result = pstats.Stats(path)
+    result.sort_stats('tottime')
+    # limit number of calls
+    # TODO: REMOVE THIS HACK
+    result.fcn_list = result.fcn_list[0:500]
+    result.print_stats()
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog='baw_cprofile_show')
+    parser.add_argument(
+        'files',
+        help='files to evaluate',
+        nargs='+',
+    )
+    # TODO: ADD ARG TO SHOW AS HTML
+    return parser
--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
         entry_points={
             'console_scripts': [
                 'baw = baw.run:main',
+                'baw_cprofile_show = baw.small.cprofile:main',
                 'baw_profile = baw.small.profile:main',
                 'baw_regen = baw.small.regen:main',
                 'baw_single = baw.small.single:main',
