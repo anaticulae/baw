073faf5a699f11962655342170187e689ea6dafb
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-09-15 11:03:32 +0200

    feat(pipeline): add jenkins init

--- baw/cmd/pipeline.py
+++ baw/cmd/pipeline.py
@@ -7,6 +7,42 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import os
+
+import baw.git
+import baw.resources
+import baw.utils
+
+
+def init(
+    root: str,
+    verbose: bool = False,
+    venv: bool = False,
+):
+    source = jenkinsfile(root)
+    replaced = baw.resources.template_replace(
+        root,
+        template=baw.resources.JENKINSFILE,
+    )
+    with baw.git.git_stash(root, verbose=verbose, virtual=venv):
+        baw.utils.file_create(
+            source,
+            content=replaced,
+        )
+        failure = baw.git.git_commit(
+            root,
+            source=source,
+            message='chore(ci): add Jenkinsfile',
+            verbose=verbose,
+        )
+        if failure:
+            return failure
+    return baw.utils.SUCCESS
+
+
+def jenkinsfile(root: str):
+    return os.path.join(root, 'Jenkinsfile')
+
 
 def extend_cli(parser):
     sync = parser.add_parser('pipeline', help='Run pipline task')
