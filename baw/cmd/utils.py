6dd3946af59f241f5a4353e4f4e077f7f3383215
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-10-11 18:49:58 +0200

    refactor(utils): move code to a more general place

---
+++ baw/cmd/utils.py
@@ -0,0 +1,53 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+import sys
+
+import baw.config
+import baw.project
+import baw.utils
+
+
+def get_root(args):
+    directory = run_environment(args)
+    root = determine_root(directory)
+    if not root:
+        return sys.exit(baw.utils.FAILURE)
+    return root
+
+
+def run_environment(args):
+    if baw.config.venv_always():
+        # overwrite venv selection
+        args['venv'] = True
+    root = setup_environment(
+        args['raw'],
+        args.get('venv', False),
+    )
+    return root
+
+
+def setup_environment(raw, venv):  # pylint: disable=W0621
+    if venv:
+        # expose venv flag
+        os.environ['VENV'] = "TRUE"
+    if raw:
+        # expose raw out flag
+        os.environ[baw.utils.PLAINOUTPUT] = "TRUE"
+    root = os.getcwd()
+    return root
+
+
+def determine_root(directory):
+    root = baw.project.determine_root(directory)
+    if not root:
+        baw.utils.error('require .baw file')
+        return None
+    return root
