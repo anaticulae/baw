fa3b9378e190169da3ac02df772f6300576f237d
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-10-16 12:24:44 +0200

    test(pipe): add unit tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -9,7 +9,34 @@
 
 import os
 
+import pytest
+
 
 def test_cmd_pipeline_init(simple):
     simple[0]('pipe init')
     assert os.path.exists(simple[1].join('Jenkinsfile'))
+
+
+def test_cmd_pipeline_lib_error(simple, capsys):
+    with pytest.raises(AssertionError, match='ExceptionInfo SystemExit'):
+        simple[0]('pipe library')
+    assert not os.path.exists(simple[1].join('Jenkinsfile'))
+    assert 'could not find Jenkinsfile' in stderr(capsys)
+
+
+def test_cmd_pipeline_lib_upgrade(simple):
+    simple[0]('pipe init')
+    # upgrade
+    simple[0]('pipe library')
+
+
+def test_cmd_pipeline_lib_already_upgraded(simple, capsys):
+    simple[0]('pipe init')
+    simple[0]('pipe library')  # upgrade
+    with pytest.raises(AssertionError, match='ExceptionInfo SystemExit'):
+        simple[0]('pipe library')  # already upgraded
+    assert 'already newst caelum' in stderr(capsys)
+
+
+def stderr(capsys):
+    return capsys.readouterr().err
