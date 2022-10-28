2d7170a6a2e96354591a000930b17904b11de12c
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-10-28 16:46:15 +0000

    test(cli): add refactor unit test

---
+++ tests/test_refactor.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import utilatest
+
+
+@utilatest.longrun
+def test_cmd_refactor(simple, capsys):
+    simple[0]('refactor')
+    log = utilatest.stdout(capsys)
+    assert 'refactor:' in log
