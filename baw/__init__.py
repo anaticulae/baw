1a71fbcd271d311bc4479f8d040d35afa86a6f2e
Automated Release <automated_release@ostia.la>
2022-11-22 13:42:03 +0000

    1.23.0 auto generated release
    >>> Feature
    
    * integrate chore into patch category (07095dcf4666)

--- baw/__init__.py
+++ baw/__init__.py
@@ -9,7 +9,7 @@
 
 ROOT = None
 
-__version__ = '1.22.1'
+__version__ = '1.23.0'
 
 import baw.__patch__  # pylint:disable=wrong-import-position
 import baw.__root__  # pylint:disable=wrong-import-position
