5740b626cc9971d3574b5574b234d3c1e9dfc6af
Automated Release <automated_release@ostia.la>
2022-11-22 13:15:40 +0000

    1.22.1 auto generated release

--- baw/__init__.py
+++ baw/__init__.py
@@ -9,7 +9,7 @@
 
 ROOT = None
 
-__version__ = '1.22.0'
+__version__ = '1.22.1'
 
 import baw.__patch__  # pylint:disable=wrong-import-position
 import baw.__root__  # pylint:disable=wrong-import-position
