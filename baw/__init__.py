708933619bbd94301ebd9d11429b331b6ccf21a2
Automated Release <automated_release@ostia.la>
2022-12-05 17:01:50 +0000

    1.30.0 auto generated release
    >>> Feature
    
    * add option to determine newest image (fdbd7cfe69e2)

--- baw/__init__.py
+++ baw/__init__.py
@@ -9,7 +9,7 @@
 
 ROOT = None
 
-__version__ = '1.29.0'
+__version__ = '1.30.0'
 
 import baw.__patch__  # pylint:disable=wrong-import-position
 import baw.__root__  # pylint:disable=wrong-import-position
