25e06b6283087038216ebd9e13820995dcc2efad
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-20 01:12:41 +0000

    test(release): verify release cmd

--- tests/test_release.py
+++ tests/test_release.py
@@ -7,6 +7,11 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import os
+
+import baw.cmd.release.pack
+import baw.git
+import baw.utils
 import tests
 
 
@@ -14,3 +19,18 @@ def test_release_already_done(simple, capsys):
     simple[0]('--verbose release')
     stdout = tests.stdout(capsys)
     assert 'head is already: v0.0.0' in stdout
+
+
+@tests.longrun
+def test_release_simple(simple, monkeypatch, capsys):
+    root = simple[1]
+    path = os.path.join(root, 'abc.txt')
+    baw.utils.file_create(path, 'CONTENT')
+    baw.git.add(root, 'abc.txt')
+    baw.git.commit(root, '.', 'feat(abc): feature is welcome')
+    with monkeypatch.context() as patched:
+        # do not open ide
+        patched.setattr(baw.cmd.release.pack, 'release_cmd', lambda _, __: 'ls')
+        simple[0]('--verbose release')
+    stdout = tests.stdout(capsys)
+    assert 'Completed: `ls` in' in stdout
